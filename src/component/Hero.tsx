08a3ed9239dea99e6c98279d95df25cd8f710412
Hasan <sohagali.ru.ac@gmail.com>
2025-01-25 23:59:30 +0600

    add writer

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -7,6 +7,7 @@ import about from "@/assets/banner (2).png";
 import {
   ArrowDownToLine,
   FacebookIcon,
+  Github,
   GithubIcon,
   Linkedin,
 } from "lucide-react";
@@ -72,8 +73,8 @@ const Hero = () => {
                   <h2 className="text-xl font-medium tracking-wide text-blue-300">
                     Hey, I&apos;m Hasan Ali
                   </h2>
-                  <h1 className="text-5xl md:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#72c4f2] to-[#e7dbfd] mt-10">
-                    Full Stack Developer
+                  <h1 className="text-4xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#72c4f2] to-[#e7dbfd] mt-10">
+                    MERN Stack Developer
                   </h1>
                 </div>
 
@@ -85,11 +86,40 @@ const Hero = () => {
 
                 {/* Social Links */}
 
-                <div className="flex items-center space-x-4 pt-6">
-                  <button className="bg-[#027bc2] hover:bg-blue-700 text-white px-6 py-3 rounded-lg transition duration-300 flex items-center space-x-2">
-                    <span>Download CV</span>
-                    <ArrowDownToLine size={20} />
-                  </button>
+                <div className="flex items-center gap-8  pt-6">
+                  <div className="flex items-center space-x-4 ">
+                    <button className="bg-[#027bc2] hover:bg-blue-700 text-white px-6 py-3 rounded-lg transition duration-300 flex items-center space-x-2">
+                      <span>Download CV</span>
+                      <ArrowDownToLine size={20} />
+                    </button>
+                  </div>
+                  <div className="flex gap-5">
+                    <Link
+                      href="https://www.facebook.com/mdhasan.alikhan.794"
+                      target="_blank"
+                    >
+                      <div className="border border-[#027bc2] rounded-full w-[50px] h-[50px] inline-flex justify-center items-center hover:bg-[#027bc2] hover:text-white">
+                        <FacebookIcon />
+                      </div>
+                    </Link>
+
+                    <Link
+                      href="https://www.linkedin.com/in/md-hasan-ali-khan/"
+                      target="_blank"
+                    >
+                      <div className="border border-[#027bc2] rounded-full w-[50px] h-[50px] inline-flex justify-center items-center hover:bg-[#027bc2] hover:text-white">
+                        <Linkedin />
+                      </div>
+                    </Link>
+                    <Link
+                      href="https://www.linkedin.com/in/md-hasan-ali-khan/"
+                      target="_blank"
+                    >
+                      <div className="border border-[#027bc2] rounded-full w-[50px] h-[50px] inline-flex justify-center items-center hover:bg-[#027bc2] hover:text-white">
+                        <Github />
+                      </div>
+                    </Link>
+                  </div>
                 </div>
               </motion.div>
 
